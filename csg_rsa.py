6c3c1d4ab174134cf052afff5c163e969bb9e0f6
Chad Estioco <chadestioco@gmail.com>
2014-04-03 11:20:22 +0800

    Added unit tests.

--- csg_rsa.py
+++ csg_rsa.py
@@ -8,7 +8,7 @@ def rsa_sign(message, privkey_file):
     message - string
     privkey_file - file object (as returned by Python's `open` bif)
     """
-    privkey = rsa.PrivateKey.load_pkcs1(privkeyfile)
+    privkey = rsa.PrivateKey.load_pkcs1("".join(privkey_file))
     signature = rsa.sign(message, privkey, "SHA-512")
 
     return signature.encode("base64")
@@ -16,8 +16,16 @@ def rsa_sign(message, privkey_file):
 class FunctionsTest(unittest.TestCase):
     
     def test_rsa_sign(self):
-        # TODO
-        pass
+        message = "that then i'd scorn to change my state with kings"
+        expected_signed = ""
+
+        with open("signed.b64") as es:
+            expected_signed = "".join(es)
+
+        with open("private_key.pem1") as privkey_file:
+            function_signed = rsa_sign(message, privkey_file)
+
+            self.assertEquals(expected_signed, function_signed)
 
 if __name__ == "__main__":
     unittest.main()
